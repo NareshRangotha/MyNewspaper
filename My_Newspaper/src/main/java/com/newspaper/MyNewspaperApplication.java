ad8cd8ef0fe527c3f5dd2c7b0d5b5c5d71d4dbe8
Naresh Rangotha <naresh.rangotha@gmail.com>
2022-11-20 10:17:40 +0530

    Revert "application"
    This reverts commit acd180ef1e5014fb70cd3c0639fdf9e07a2e50e8.

--- My_Newspaper/src/main/java/com/newspaper/MyNewspaperApplication.java
+++ My_Newspaper/src/main/java/com/newspaper/MyNewspaperApplication.java
@@ -10,10 +10,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class MyNewspaperApplication implements CommandLineRunner {
 
-	@Autowired
-	private IUserService userService;
-	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+//	@Autowired
+//	private IUserService userService;
+//	@Autowired
+//	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyNewspaperApplication.class, args);
@@ -22,17 +22,17 @@ public class MyNewspaperApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		User user = new User();
-		user.setFirstName("Admin");
-		user.setLastName("Service");
-		user.setEmail("admin@gmail.com");
-		user.setPassword("123");
-		user.setPassword(this.bCryptPasswordEncoder.encode(user.getPassword()));
-		user.setContact("8878821887");
-		user.setUserName("Admin123");
-		user.setRole("ADMIN");
-		user.setEnabled(true);
-		this.userService.createUser(user);
+//		User user = new User();
+//		user.setFirstName("Admin");
+//		user.setLastName("Service");
+//		user.setEmail("admin@gmail.com");
+//		user.setPassword("123");
+//		user.setPassword(this.bCryptPasswordEncoder.encode(user.getPassword()));
+//		user.setContact("8878821887");
+//		user.setUserName("Admin123");
+//		user.setRole("ADMIN");
+//		user.setEnabled(true);
+//		this.userService.createUser(user);
 
 	}
 
